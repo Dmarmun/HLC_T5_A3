697b11681faed14ef7fefd706405cb9d6c0e436a
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-29 12:29:45 +0100

    Ejercicio 1 corregido y ejercicio 2 terminado

--- e001.py
+++ e001.py
@@ -1,4 +1,4 @@
 lista = [5, 2, 9, 1, 6, 7, 3, 8, 4]
-print("La lista sin ordenar: "+lista)
+print("La lista sin ordenar: "+str(lista))
 lista.sort()
-print("La lista ordenada: "+lista)
+print("La lista ordenada: "+str(lista))
---
+++ e002.py
@@ -0,0 +1,4 @@
+lista = ['hola', 'hola', 'caracola', 'caracola']
+print("La lista sin ordenar: "+str(lista))
+sin_repes = list(set(lista))
+print("La lista ordenada: "+str(sin_repes))
