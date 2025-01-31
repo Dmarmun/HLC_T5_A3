0d96e8982f29f5bd893532d9c05a389d1256a5d1
Dmarmun <dmarmun0704@g.educaand.es>
2025-01-31 11:37:17 +0100

    Ejercicio 2 corregido

--- e002.py
+++ e002.py
@@ -1,4 +1,6 @@
+def ordenada(lista):
+    sin_repes = list(set(lista))
+    print("La lista ordenada: "+str(sin_repes))
 lista = ['hola', 'hola', 'caracola', 'caracola']
 print("La lista sin ordenar: "+str(lista))
-sin_repes = list(set(lista))
-print("La lista ordenada: "+str(sin_repes))
+ordenada(lista)
